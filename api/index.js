d569c9bb75e880d1e5f2dbcd39a4c0311fbcf180
matttre3 <140153086+matttre3@users.noreply.github.com>
2024-02-09 20:45:10 +0100

    prova 2 api

--- api/index.js
+++ api/index.js
@@ -1,16 +1,12 @@
-const app = require('express')();
-const { v4 } = require('uuid');
+// Add Express
+const express = require("express");
 
-app.get('/api', (req, res) => {
-  const path = `/api/item/${v4()}`;
-  res.setHeader('Content-Type', 'text/html');
-  res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
-  res.end(`Hello! Go to item: <a href="${path}">${path}</a>`);
-});
+// Initialize Express
+const app = express();
 
-app.get('/api/item/:slug', (req, res) => {
-  const { slug } = req.params;
-  res.end(`Item: ${slug}`);
+// Create GET request
+app.get("/", (req, res) => {
+  res.send("Express on Vercel");
 });
 
 app.get('/api/ciola', (req, res) => {
@@ -20,6 +16,13 @@ app.get('/api/ciola', (req, res) => {
     });
 });
 ``
+
+// Initialize server
+app.listen(5000, () => {
+  console.log("Running on port 5000.");
+});
+
+
 
 
 module.exports = app;
