56eeaef3d4356f9bb75754fc5f4e766f2012eb36
matttre3 <140153086+matttre3@users.noreply.github.com>
2024-02-09 21:07:00 +0100

    cors changes

--- api/index.ts
+++ api/index.ts
@@ -1,7 +1,15 @@
 
 import express from 'express'
+import cors from 'cors'
+
+
+var corsOptions = {
+  origin: 'sushe-backend-p9to.vercel.app',
+  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+}
 
 const app = express()
+app.use(cors(corsOptions))
 app.get("/api/ciola",(req,res) => {
   res.json({
     nome: 'Matteo',
