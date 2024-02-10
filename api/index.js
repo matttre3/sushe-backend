f3148d754b506fc7423726612a90186d60b9fa60
matttre3 <140153086+matttre3@users.noreply.github.com>
2024-02-10 11:03:07 +0100

    prova create table

--- api/index.js
+++ api/index.js
@@ -1,5 +1,11 @@
 const express = require('express')
 const cors = require('cors')
+const { sql } = require("@vercel/postgres");
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  connectionString: 'postgres://default:jKZEJNap15rX@ep-shy-bonus-a4wq96h7-pooler.us-east-1.aws.neon.tech/verceldb?sslmode=require' ,
+})
 
 var corsOptions = {
   origin: 'https://sushe-app.vercel.app',
@@ -14,5 +20,29 @@ app.get("/api/ciola",(req,res) => {
     cognome: 'Romano'
 });
 })
+
+app.get("/api/crea-tabella-utenti", async (req, res) => {
+  const client = await pool.connect();
+
+  try {
+    const createTableQuery = `
+      CREATE TABLE IF NOT EXISTS users (
+        id SERIAL PRIMARY KEY,
+        user VARCHAR(255) NOT NULL,
+        password VARCHAR(255) NOT NULL
+      )
+    `;
+    
+    await client.query(createTableQuery);
+    res.json({ message: 'Tabella utenti creata con successo' });
+  } catch (error) {
+    console.error('Errore durante la creazione della tabella:', error);
+    res.status(500).json({ error: 'Errore interno del server' });
+  } finally {
+    client.release();
+  }
+});
+
+
 
 module.exports = app
