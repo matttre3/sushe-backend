dd19f163a62f743d6c55b40528457af788e710f8
matttre3 <140153086+matttre3@users.noreply.github.com>
2024-02-10 13:29:09 +0100

    asdsadsa

--- api/db.js
+++ api/db.js
@@ -9,12 +9,9 @@ const config = isDevMode()?{
   port: 5432, // default Postgres port
   database: 'myapp'
 }:{
-  user: 'default',
-  password: 'jKZEJNap15rX',
-  host: 'ep-shy-bonus-a4wq96h7.us-east-1.aws.neon.tech',
-  port: 5432, // default Postgres port
-  database: 'verceldb'
+    connectionString: process.env.POSTGRES_URL,
 }
+
 
 const pool = new Pool(config)
 
