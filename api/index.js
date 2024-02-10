69c6e197d3306519bcc1c42b7d3af9fcb5349826
matttre3 <140153086+matttre3@users.noreply.github.com>
2024-02-10 10:46:29 +0100

    tb

--- api/index.js
+++ api/index.js
@@ -1,29 +1,5 @@
 const express = require('express')
 const cors = require('cors')
-const sql = require('@vercel/postgre')
-const { Pool } = require('pg');
-
-// Configura il pool di connessioni
-const pool = new Pool({
-  connectionString: process.env.POSTGRES_URL,
-  ssl: {
-    rejectUnauthorized: false, // Assicurati di impostare questo valore correttamente in produzione
-  },
-});
-
-// Test della connessione
-pool.connect((err, client, release) => {
-  if (err) {
-    return console.error('Errore durante la connessione al database:', err);
-  }
-  console.log('Connessione al database avvenuta con successo');
-
-  // Esegui query o altre operazioni qui
-
-  // Rilascia la connessione
-  release();
-});
-
 
 var corsOptions = {
   origin: 'https://sushe-app.vercel.app',
